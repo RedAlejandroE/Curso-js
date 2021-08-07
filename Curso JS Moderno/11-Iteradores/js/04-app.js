802e43e50a2775ae2682876e0bc5d68c98d9bbbc
Alejandro Escobar <alejandrod.escobar.20@gmail.com>
2021-08-07 02:59:08 -0500

    Iteradores

---
+++ Curso JS Moderno/11-Iteradores/js/04-app.js
@@ -0,0 +1,28 @@
+// Otro iterador muy común es el while loop, este se ejecuta mientras una condición sea verdadera..
+
+let i = 0;
+while (i < 10) {
+  // condicion
+
+  // Bloque de código...
+  console.log(`Numero: ${i}`);
+
+  i++; // incremento
+}
+
+// El while se ejecuta mientras una condición sea verdadera, por lo tanto si inicicializamos en 20, no hará nada..
+
+/* Intenta realizar los mismos ejemplos  Detectar pares y nones y el fizz buzz con el while, seguramente va a quedar mucho mas claro... */
+
+let i = 1;
+while (i <= 100) {
+  if (i % 15 === 0) {
+    console.log(`${i} FIZZ BUZZ`);
+  } else if (i % 3 === 0) {
+    console.log(`${i} FIZZ`);
+  } else if (i % 5 === 0) {
+    console.log(`${i} BUZZ`);
+  }
+
+  i++;
+}
